4d07696ff30010e1cd8c275d321fe7d08ef08176
Skills Academy <student03@MckenzieCamacho.fios-router.home>
2022-04-18 10:14:46 -0400

    dependencies and components

---
+++ studio-ghibli/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
+import Header from './components/Header'
+import Homepage from './components/Homepage'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+    
+
+    </div>
+  );
+}
+
+export default App;
